a8808499be4b609bf55c0282c706dcf7aabdaf59
Blackfml <lencinadavaloa@gmail.com>
2026-06-30 16:29:09 -0300

    fix Vercel: use standard Express export pattern (no async wrapper)

--- api/index.ts
+++ api/index.ts
@@ -1,12 +1,3 @@
 import app from "../server/app";
-import { ensureInit } from "../server/services/init";
 
-let initialized = false;
-
-export default async function handler(req: any, res: any) {
-  if (!initialized) {
-    initialized = true;
-    await ensureInit();
-  }
-  return app(req, res);
-}
+export default app;
